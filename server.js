0503f1aab121cf2cdbb2c36d2fe0dfeec651ebd7
osvaldo24568-beep <osvaldo24568@cbtis75.edu.mx>
2026-05-07 16:27:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
